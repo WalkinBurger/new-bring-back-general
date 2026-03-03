ecf75c78cd121a9cbd020c67f774a51f8b080907
WalkinBurger <nch720@proton.me>
2026-03-03 16:59:59 +0000

    idk

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as bs, element
 import fileinput
 import time
 import datetime
+import json
 
 start = time.time()
 
@@ -50,7 +51,7 @@ def msg_format(i):
 
 # file starts at 8 & ends at 166080
 LINE_START = 8
-LINE_END = 166080
+LINE_END = 18
 
 line_count = 0
 msg_count = 0
@@ -60,9 +61,14 @@ error_lines = {}
 cleared = True
 last_progress = 0
 progress = 0
+msgs = []
+all_msgs = {}
 multiline_buffer = ""
 with open("errorLog.html", 'w') as error_log:
     error_log.write('')
+for i in range(10):
+    with open(f"output/log{i}.json", 'w') as output:
+        output.write('')
 
 try:
     for msg_group in fileinput.input(["fullLog.html"], encoding="utf-8", mode='r'):
@@ -79,11 +85,13 @@ try:
             multiline_buffer = ""
 
         msg_group_count += 1
+        msgs = []
 
         progress = line_count / 166080 
         if progress - last_progress > 0.1:
             print(f"{int(progress * 100)}% - {line_count} / 166080")
             last_progress = progress
+            all_msgs = {}
 
         inner_msg_count = 0
         soup = bs(msg_group, "html.parser")
@@ -95,18 +103,28 @@ try:
             else:
                 raise
         name = soup.find("span", class_="chatlog__author").text
-        # print(name, avatar)
+
+        try:
+            reply = soup.find("span", class_="chatlog__reference-link")
+            reply_id = str(reply["onclick"]).split("'")[1]
+            reply_author = soup.find("div", class_="chatlog__reference-author").text
+            reply_text = f"> {reply_author}: \n> {all_msgs[reply_id]}\n"
+        except TypeError:
+            reply_text = ""
+
         for msg in soup.find_all("div", class_="chatlog__message-primary"): 
             inner_msg_count += 1
             msg_count += 1
+            msg_id = msg.parent.parent["data-message-id"]
             md = msg.find("span", class_="chatlog__markdown-preserve")
             links = msg.find_all("img", {"class": ["chatlog__attachment-media", "chatlog__embed-generic-image", "chatlog__sticker--media"]}) + msg.find_all("source", {"alt": ["Video attachment", "Embedded video", "Audio attachment"]})
             midi = msg.find_all("div", class_="chatlog__attachment-generic-name")
-            msg_text = ""
+            msg_text = reply_text
             stickers = msg.find_all("div", class_="chatlog__sticker--media")
+
             if md:
                 for i in md:
-                    msg_text += msg_format(i)
+                    msg_text += f" {msg_format(i)}"
             else:
                 embed = msg.find_all("a", class_="chatlog__embed-image-link")
                 md = msg.find_all("div", class_="chatlog__markdown-preserve")
@@ -117,26 +135,39 @@ try:
                     links += msg.find_all("img", class_="chatlog__embed-image-link")
                 if embed:
                     for i in embed:
-                        msg_text += i["href"]
+                        msg_text += f" {i["href"]}"
             if midi:
                 for i in midi:
-                    msg_text += i.contents[0]["href"]
+                    msg_text += f" {i.contents[0]["href"]}"
             if links:
                 for i in links:
-                    msg_text += i["src"]
+                    msg_text += f" {i["src"]}"
             if stickers:
                 for i in stickers:
-                    msg_text += f"Stick: {i.parent["title"]}"
+                    msg_text += f"Sticker: {i.parent["title"]}"
             # print(msg_text)
+            if len(msg_text) != 0 and msg_text[0] == ' ':
+                msg_text = msg_text[1:]
+            msgs.append(msg_text)
+            all_msgs[msg_id] = msg_text
             if msg_text == "":
                 try:
                     soup.find("span", class_="chatlog__bot-label")
                     potential_error_lines[line_count] = inner_msg_count
                 except:
                     error_lines[line_count] = inner_msg_count
-                    with open("errorLog.html", 'a') as error_log:
-                        error_log.write(soup.prettify())
-                        error_log.writelines("\n\n")
+            with open("errorLog.html", 'a') as error_log:
+                error_log.write(soup.prettify())
+                error_log.writelines("\n\n")
+
+        with open(f"output/log{int(progress * 10)}.json", 'a', encoding="utf-8") as output:
+            json.dump({
+                msg_group_count: {
+                    "name": str(name),
+                    "avatar": str(avatar),
+                    "msgs": msgs
+                }
+            }, output, indent=4, ensure_ascii=False)
 
         if line_count >= LINE_END:
             # print(f"Last message: {msg_group}")
